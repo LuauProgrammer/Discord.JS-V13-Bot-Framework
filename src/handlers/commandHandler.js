fe255a1be65f78fc1b31730a0dad89914674aa37
LuauProgrammer <aydentaylorl@gmail.com>
2022-03-09 22:49:12 -0500

    Remove some dev comments

--- src/handlers/commandHandler.js
+++ src/handlers/commandHandler.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 module.exports = (client, Discord) => {
     const commandFiles = fs
         .readdirSync("src/commands")
-        .filter(file => file.endsWith(".js")); // Get and filter all the files in the "Commands" Folder.
+        .filter(file => file.endsWith(".js"));
     for (const file of commandFiles) {
-        const command = require(`../commands/${file}`); // Get and define the command file.
-        client.commands.set(command.data.name, command); // Set the command name and file for handler to use.
-        client.commandarray.push(command.data.toJSON()); // Push the command data to an array (for sending to the API).
+        const command = require(`../commands/${file}`);
+        client.commands.set(command.data.name, command);
+        client.commandarray.push(command.data.toJSON());
     }
 }
