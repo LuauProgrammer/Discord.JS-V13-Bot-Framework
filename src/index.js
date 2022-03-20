2cf8d2fd92a6801c594ba08e643b0e66d3c1302d
LuauProgrammer <aydentaylorl@gmail.com>
2022-03-19 23:46:05 -0400

    Add Sharding

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
-const Discord = require("discord.js")
-const { Client, Intents, Collection } = Discord
-const botClient = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
-});
+const { ShardingManager } = require('discord.js');
+const config = require("../config.json")
+const manager = new ShardingManager('./src/bot.js', { token: process.env.token || config.token });
 
-botClient.config = require("../config.json")
-botClient.commands = new Collection();
-botClient.commandarray = [];
+manager.on('shardCreate', shard => console.log(`Launched shard #${shard.id}`));
 
-['commandHandler', 'eventHandler'].forEach(handler => {
-  require(`./handlers/${handler}`)(botClient, Discord);
-})
-
-
-botClient.login(process.env.token || botClient.config.token); 
+manager.spawn();
